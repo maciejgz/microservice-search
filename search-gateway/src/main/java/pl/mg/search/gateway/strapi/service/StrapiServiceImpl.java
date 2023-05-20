8e6320f0d82f4632b7bf602510b3fe078d238293
maciejgz <macgzi@gmail.com>
2023-05-20 05:40:22 +0200

    CMS model - merge

--- search-gateway/src/main/java/pl/mg/search/gateway/strapi/service/StrapiServiceImpl.java
+++ search-gateway/src/main/java/pl/mg/search/gateway/strapi/service/StrapiServiceImpl.java
@@ -200,7 +200,6 @@ public class StrapiServiceImpl implements StrapiService {
                         Optional<String> image = findImageId(split[i]);
                         image.ifPresent(s -> imageees.add(new Image(Integer.parseInt(s))));
                     }
-                    //release changes
                     if (CollectionUtils.isNotEmpty(imageees)) {
                         Image[] array = new Image[imageees.size()];
                         cmsModel.setImages(imageees.toArray(array));
