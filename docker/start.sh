5879fd807d0fc99d4d002cee14e08343a3a701a9
maciejgz <macgzi@gmail.com>
2023-09-02 20:22:49 +0200

    #7 project modules renamed
    - projects refactoring
    - ms-api-gateway module
    - single api-gateway instance

--- docker/start.sh
+++ docker/start.sh
@@ -3,11 +3,10 @@ docker-compose -f db.yml -p microservice-search up -d
 docker-compose -f mongodb.yml -p microservice-search up -d
 docker-compose -f kafka.yml -p microservice-search up -d
 
-
 for i in {1..3}; do
-  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d--scale ms-cms-service=$i
-  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d--scale ms-stock-service=$i
-  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d--scale ms-search-gateway=$i
+  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d --scale ms-cms-service=$i
+  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d --scale ms-stock-service=$i
+  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d --scale ms-api-gateway=1
   sleep 5
 done
 
