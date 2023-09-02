9fd7254499d2e8c28865d56852aafad7b46a3a49
maciejgz <macgzi@gmail.com>
2023-09-02 14:38:50 +0200

    #9 docker compose build
    -docker compose finished

--- docker/start.sh
+++ docker/start.sh
@@ -2,5 +2,12 @@
 docker-compose -f db.yml -p microservice-search up -d
 docker-compose -f mongodb.yml -p microservice-search up -d
 docker-compose -f kafka.yml -p microservice-search up -d
-docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d
+
+
+for i in {1..3}; do
+  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d--scale ms-cms-service=$i
+  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d--scale ms-stock-service=$i
+  docker-compose -f app.yml -p %COMPOSE_PROJECT_NAME% up -d--scale ms-search-gateway=$i
+  sleep 5
+done
 
