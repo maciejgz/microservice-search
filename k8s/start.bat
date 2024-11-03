272b45103481fcaacf1d084ac0b5a541104b4c19
maciejgz <macgzi@gmail.com>
2024-11-03 18:52:26 +0100

    #17 3rd party services in k8s

--- k8s/start.bat
+++ k8s/start.bat
@@ -1,5 +1,12 @@
 @echo off
+
 kubectl apply -f k8s
+kubectl apply -f k8s/postgres
+kubectl apply -f k8s/kafka
+kubectl apply -f k8s/keycloak
+kubectl apply -f k8s/redis
+
+
 kubectl apply -f ms-admin/k8s
 kubectl apply -f ms-api-gateway/k8s
 kubectl apply -f ms-cms-service/k8s
